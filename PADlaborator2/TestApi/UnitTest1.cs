4e02a687e84a909da3942c3958d828c6292a47f2
vlada061098@gmail.com <vlada061098@gmail.com>
2020-11-19 00:23:19 +0200

    Add test project

---
+++ PADlaborator2/TestApi/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using PADLab2_1part.Controllers;
+
+namespace TestApi
+{
+    public class UnitTest1
+    {
+
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
